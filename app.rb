4f558ca3bf1ee1eaf55d22eabc79dca62fdca644
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-24 17:32:20 +0100

    Sinatra: post ed params

--- app.rb
+++ app.rb
@@ -24,8 +24,11 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  p params
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   @name = params[:name]
   erb(:index)
 end
