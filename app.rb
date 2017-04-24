379dc1810433a5d1953293311eccf3c08103fe41
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-24 14:21:06 +0100

    Sinatra returning HTML

---
+++ app.rb
@@ -0,0 +1,26 @@
+
+require 'sinatra'
+require 'shotgun'
+set :session_secret, 'here be dragons'
+
+get '/' do
+  'Hello World!'
+end
+
+get '/secret' do
+  "secret message"
+end
+
+get '/shotgun' do
+  "shotgun"
+end
+
+get '/new' do
+  "new"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
